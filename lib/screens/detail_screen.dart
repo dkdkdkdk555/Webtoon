8b1903ba7a3ebfb21fee943cb740853586aa1ff8
dkdkdkdk555 <dkdkdkdk555@naver.com>
2024-03-03 11:25:42 +0900

    widget.프로퍼티

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:webtoon/models/webtoon_detail_model.dart';
+import 'package:webtoon/models/webtoon_episode_model.dart';
+import 'package:webtoon/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
+
 
   const DetailScreen({super.key,
    required this.title,
    required this.thumb, 
    required this.id});
 
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
 
+class _DetailScreenState extends State<DetailScreen> {
+  /*
+    widget.id 이런 식으로 State 내부에서 위 Statefull위젯의 맴버들을
+    참조할 수 있는 이유는 State에서 자신이 속한 스테이트풀위젯의 데이터를 받아와서 가능하다.
+   */
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +40,7 @@ class DetailScreen extends StatelessWidget {
         elevation: 2, // 앱바 경계선의 음영?
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
-        title: Text(title,
+        title: Text(widget.title,
           style: const TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w600,
@@ -35,7 +56,7 @@ class DetailScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Hero(
-                tag: id,
+                tag: widget.id,
                 child: Container(
                   width: 250,
                   clipBehavior: Clip.hardEdge, // clipBehavior는 자식이 부모 영역을 침범하는 속성
@@ -50,7 +71,7 @@ class DetailScreen extends StatelessWidget {
                     ]
                   ),
                   child: Image.network(
-                    thumb,
+                    widget.thumb,
                     headers: const { 
                       // 밑에는 안드로이드 나 IOS 를 에뮬레이터를 사용할 경우 CORS 문제를 해결하기 위해 넣어야 하는 헤더 내용이다.
                       // 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
